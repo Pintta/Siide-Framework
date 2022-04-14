29652af7c40ba61c67a5ae13408dc2ceaa910406
Pintta <69728770+Pintta@users.noreply.github.com>
2022-04-14 20:19:39 +0300

    Update proximity.lua

--- data/resources/[speak]/pma-voice/client/init/proximity.lua
+++ data/resources/[speak]/pma-voice/client/init/proximity.lua
@@ -1,4 +1,3 @@
--- used when muted
 local disableUpdates = false
 local isListenerEnabled = false
 
@@ -7,23 +6,18 @@ function addNearbyPlayers()
 	local coords = GetEntityCoords(PlayerPedId())
 	local voiceModeData = Cfg.voiceModes[mode]
 	local distance = GetConvar('voice_useNativeAudio', 'false') == 'true' and voiceModeData[1] * 3 or voiceModeData[1]
-
 	MumbleClearVoiceTargetChannels(voiceTarget)
 	local players = GetActivePlayers()
 	for i = 1, #players do
 		local ply = players[i]
 		local serverId = GetPlayerServerId(ply)
-
 		if serverId == playerServerId then goto skip_loop end
-
 		local ped = GetPlayerPed(ply)
 		if #(coords - GetEntityCoords(ped)) < distance then
 			if isTarget then goto skip_loop end
-
 			logger.verbose('Added %s as a voice target', serverId)
 			MumbleAddVoiceTargetChannel(voiceTarget, serverId)
 		end
-
 		::skip_loop::
 	end
 end
@@ -67,7 +61,6 @@ RegisterNetEvent('onPlayerDropped', function(serverId)
 	end
 end)
 
--- cache talking status so we only send a nui message when its not the same as what it was before
 local lastTalkingStatus = false
 local lastRadioStatus = false
 Citizen.CreateThread(function()
@@ -76,7 +69,6 @@ Citizen.CreateThread(function()
 		{ name = "duration", help = "(opt) the duration the mute in seconds (default: 900)" }
 	})
 	while true do
-		-- wait for mumble to reconnect
 		while not MumbleIsConnected() do
 			Wait(100)
 		end
@@ -84,10 +76,7 @@ Citizen.CreateThread(function()
 			if lastRadioStatus ~= radioPressed or lastTalkingStatus ~= (MumbleIsPlayerTalking(PlayerId()) == 1) then
 				lastRadioStatus = radioPressed
 				lastTalkingStatus = MumbleIsPlayerTalking(PlayerId()) == 1
-				SendNUIMessage({
-					usingRadio = lastRadioStatus,
-					talking = lastTalkingStatus
-				})
+				SendNUIMessage({usingRadio = lastRadioStatus, talking = lastTalkingStatus})
 			end
 		end
 		addNearbyPlayers()
@@ -97,7 +86,6 @@ Citizen.CreateThread(function()
 		elseif not isSpectating and isListenerEnabled then
 			setSpectatorMode(false)
 		end
-
 		Wait(GetConvarInt('voice_refreshRate', 200))
 	end
 end)
